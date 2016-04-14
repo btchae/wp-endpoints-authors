78bc21fb432ea86087ac15341d06e2e1530787d6
Caridad Zapatero Contreras <zzuga@zzuga.com>
2016-04-14 19:37:38 +0200

    First working version

---
+++ src/Authors/Filter.php
@@ -0,0 +1,11 @@
+<?php namespace Lean\Endpoints\Authors;
+
+/**
+ * Custom filters used on this endpoint.
+ *
+ * @package Endpoints\Authors;
+ * @since 0.1.0
+ */
+class Filter {
+	const ITEM_FORMAT = 'ln_endpoints_authors_item';
+}
